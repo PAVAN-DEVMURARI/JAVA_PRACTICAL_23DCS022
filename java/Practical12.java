2128c10d61917d1d196109d4c212f9d05e97f5b4
PAVAN-DEVMURARI <devmuraripavan556@gmail.com>
2024-07-30 09:25:00 +0530

    Added third set of practicle

---
+++ java/Practical12.java
@@ -0,0 +1,11 @@
+
+public class Practical12 {
+    public static void main(String[] args) {
+     
+			int a = Integer.parseInt(args[0]);
+			int b = a * 100;
+			System.out.println("Amount in rupees: " + b);
+		
+        
+    }
+}
